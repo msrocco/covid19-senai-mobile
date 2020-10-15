23cecb961f5c4ec1633d6005328103696119262f
Matheus Rocco <matheushrocco@gmail.com>
2020-10-15 20:01:33 -0300

    new endpoints added

--- src/services/ServiceAPI.js
+++ src/services/ServiceAPI.js
@@ -8,7 +8,27 @@ const getTotalBrazil = () => {
     return http.get("/v1/total/brazil/");
 };
 
+const getGrafico = (filtro) => {
+    return http.get(`/v1/casos/${filtro}`);
+};
+ 
+const getGraficoMortes = () => {
+    return http.get(`/v1/obitos/`);
+};
+ 
+const getGraficoInfectadosPorIdade = () => {
+    return http.get(`/v1/infectadosPorIdade/`);
+};
+ 
+const getGraficoInfectadosPorGenero = () => {
+    return http.get(`/v1/infectadosPorGenero/`);
+};
+
 export default {
     getTotalPR,
     getTotalBrazil,
+    getGrafico,
+    getGraficoMortes,
+    getGraficoInfectadosPorIdade,
+    getGraficoInfectadosPorGenero
 };

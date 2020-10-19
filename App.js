818b8fda718f11348cd713541918985c5690a889
Matheus Rocco <matheushrocco@gmail.com>
2020-10-19 16:58:37 -0300

    added new fonts

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
+import { useFonts } from 'expo-font';
+import { Nunito_400Regular, Nunito_600SemiBold, Nunito_700Bold } from '@expo-google-fonts/nunito';
+
 import AppStack from './src/routes/AppStack';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  
   return (
     <>
       <AppStack />

885e862fabb1b43bb47ec4b810e1ec8440c7ce75
Matheus Rocco <matheushrocco@gmail.com>
2020-10-21 16:31:37 -0300

    load screen

--- src/routes/AppStack.js
+++ src/routes/AppStack.js
@@ -3,13 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import BottomTabs from './BottomTabs';
 import Tweets from '../pages/Tweets';
+import LoadScreen from '../pages/LoadScreen';
 
 const { Navigator, Screen } = createStackNavigator();
 
 function AppStack() {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
+      <Navigator screenOptions={{ headerShown: false }} initialRouteName="LoadScreen" >
+        <Screen name="LoadScreen" component={LoadScreen} />
         <Screen name="BottomTabs" component={BottomTabs} />
         <Screen name="Tweets" component={Tweets} />
       </Navigator>
